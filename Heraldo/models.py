17e3c91f97c4a5fe7a6ad463ff2952cfb124b426
LuisZunigaR <luis.zuniga@ioet.com>
2023-05-15 14:48:20 -0500

    Models

--- Heraldo/models.py
+++ Heraldo/models.py
@@ -1,3 +1,27 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Rol(models.Model):
+    ROL_CHOICES = [
+        ('AD', 'Admin'),
+        ('DR', 'Drive'),
+        ('CL', 'Client'),
+    ]
+    user = models.ForeignKey(User)
+    user_rol = models.CharField(
+        max_length=2,
+        choices=ROL_CHOICES,
+        default='CL',
+    )
+
+class Driver(models.Model):
+    user = models.ForeignKey(User)
+    identification = models.CharField(max_length=30)
+    license_number = models.CharField(max_length=100)
+    status = models.ForeignKey('DriverStatus', on_delete=models.CASCADE)
+
+class DriverStatus(models.Model):
+    status = models.CharField(max_length=100)
+    description = models.CharField(max_length=300)
+
