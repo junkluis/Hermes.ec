36bf2b85647cbc15bd1bc6c93e6e0afecea8f88b
Luis Zuniga <84402053+LuisZunigaR@users.noreply.github.com>
2023-10-19 22:37:43 -0500

    4 digitos

--- Heraldo/models.py
+++ Heraldo/models.py
@@ -75,10 +75,10 @@ class Order(models.Model):
     location_coord_lat = models.CharField(max_length=255, blank=True, null=True)
     location_coord_long = models.CharField(max_length=255, blank=True, null=True)
     location_update = models.DateTimeField(auto_now=True)
-    peso = models.DecimalField(null=True, blank=True, max_digits=100, decimal_places=4)
-    tarifa = models.DecimalField(null=True, blank=True, max_digits=100, decimal_places=4)
-    precio = models.DecimalField(null=True, blank=True, max_digits=100, decimal_places=4)
-    distancia = models.DecimalField(null=True, blank=True, max_digits=100, decimal_places=4)
+    peso = models.DecimalField(null=True, blank=True, max_digits=100, decimal_places=100)
+    tarifa = models.DecimalField(null=True, blank=True, max_digits=100, decimal_places=100)
+    precio = models.DecimalField(null=True, blank=True, max_digits=100, decimal_places=100)
+    distancia = models.DecimalField(null=True, blank=True, max_digits=100, decimal_places=100)
     status = models.CharField(
         max_length=2,
         choices=ORDER_STATUS,
